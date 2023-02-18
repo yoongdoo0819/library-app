bee057e3ebadecc4c6cb5792d2a678e61eeac05e
krust <krust@krustui-MacBookPro-29.local>
2023-02-18 17:16:53 +0900

    code refactoring

--- src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
@@ -1,11 +1,34 @@
 package com.group.libraryapp.dto.user.response
 
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.loanhositry.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhositry.UserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhositry.UserLoanStatus
+
 data class UserLoanHistoryResponse (
         val name: String,
         val books: List<BookHistoryResponse>
-)
+) {
+    companion object {
+        fun of(user: User): UserLoanHistoryResponse {
+            return UserLoanHistoryResponse (
+                    name = user.name,
+                    books = user.userLoanHistories.map(BookHistoryResponse::of),
+            )
+        }
+    }
+}
 
 data class BookHistoryResponse (
         val name: String,
         val isReturn: Boolean,
-)
+) {
+    companion object {
+        fun of(history: UserLoanHistory): BookHistoryResponse {
+            return BookHistoryResponse(
+                    name = history.bookName,
+                    isReturn = history.isReturn,
+            )
+        }
+    }
+}
