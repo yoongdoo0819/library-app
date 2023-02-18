92587d767124c79babad8b1a7cd1029b3ce781a8
krust <krust@krustui-MacBookPro-29.local>
2023-02-18 18:00:04 +0900

    countLoanedBook Test & getBookStatistics Test/refactoring

--- src/main/kotlin/com/group/libraryapp/dto/book/response/BookStatResponse.kt
+++ src/main/kotlin/com/group/libraryapp/dto/book/response/BookStatResponse.kt
@@ -4,9 +4,7 @@ import com.group.libraryapp.domain.book.BookType
 
 data class BookStatResponse (
         val type: BookType,
-        var count: Int,
+        val count: Int,
 ) {
-    fun plusOne() {
-        count++
-    }
+
 }
